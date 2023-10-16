fc77735458f30f1881149880c36b66d65b2b7ca6
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-10-16 20:54:01 +0200

    feat : add name param

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,14 +1,17 @@
+import "../styles/checkbox.css";
+
 interface CheckboxProps {
-  handler: (checked: boolean, ingredients: string[]) => void;
+  handler: (checked: boolean, ingredients: string[], name: string) => void;
   ingredients: string[];
+  name: string;
 }
 
-const Checkbox = ({ handler, ingredients }: CheckboxProps) => {
+const Checkbox = ({ handler, ingredients, name }: CheckboxProps) => {
   return (
     <label className="container">
       <input
         type="checkbox"
-        onChange={(e) => handler(e.target.checked, ingredients)}
+        onChange={(e) => handler(e.target.checked, ingredients, name)}
       />
       <div className="checkmark"></div>
     </label>
