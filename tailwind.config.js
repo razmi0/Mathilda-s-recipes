0d5122cd62592ea85bb95fba4c0eaf10f400d134
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-16 12:50:39 +0100

    Update content property in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/components/*.tsx", "./src/*.tsx"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
