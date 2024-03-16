1980166d4022b7bf387dfac132a57bc3ffee605a
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-16 14:52:13 +0100

    Add "2xl" font size to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/components/*.tsx", "./src/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "2xl": "1.5rem",
+      },
+    },
   },
   plugins: [],
 };
