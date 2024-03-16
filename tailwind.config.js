54c2b64592749589430768ffeffced99662e3114
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-16 15:41:09 +0100

    Add custom colors and plugins to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,31 @@ export default {
   content: ["./src/components/*.tsx", "./src/*.tsx"],
   theme: {
     extend: {
-      cs: {
-        500: "#242424FF",
-        400: "#343434FF",
-        300: "#5F5F5FFF",
-        200: "#AEAEAEFF",
-        100: "#EBEBEBFF",
+      colors: {
+        def: {
+          500: "#242424FF",
+          400: "#343434FF",
+          300: "#5F5F5FFF",
+          200: "#AEAEAEFF",
+          100: "#EBEBEBFF",
+        },
+        blueish: {
+          500: "#1C1E24FF",
+          400: "#282C34FF",
+          300: "#3A4254FF",
+          200: "#495778FF",
+          100: "#54669DFF",
+        },
       },
       fontSize: {
         "2xl": "1.5rem",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    ({ addVariant }) => {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
 };
