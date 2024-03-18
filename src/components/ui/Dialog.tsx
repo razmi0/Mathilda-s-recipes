e4d090f30a472b8bd236222cfc32db1e394272f8
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-18 16:02:33 +0100

    Update close button color in Dialog component

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -35,7 +35,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <Icon name="cross2" className="h-4 w-4" />
+        <Icon name="cross2" color={"#AEAEAEFF"} className="h-4 w-4 hover:stroke-def-200" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
